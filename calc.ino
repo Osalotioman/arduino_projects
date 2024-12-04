2371ee8af8ba681ea59ad601993f56593c7a85ab
Sbog304 <145205923+Sbog304@users.noreply.github.com>
2024-12-04 14:02:12 +0100

    Create calc.ino

---
+++ calc.ino
@@ -0,0 +1,25 @@
+void setup() {
+  // put your setup code here, to run once:
+  // First set all the values on the light tube to show zero
+}
+
+void click1(){
+  
+}
+
+void click2(){
+  
+}
+
+void clear_screen(){
+  // Set all values on the screen to zero
+}
+
+void equal_to(){
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
