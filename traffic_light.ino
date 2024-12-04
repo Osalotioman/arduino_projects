7aa8a27e78e20c592846f4c64cb65104ba921968
Sbog304 <145205923+Sbog304@users.noreply.github.com>
2024-12-04 13:59:52 +0100

    Create traffic_light.ino

---
+++ traffic_light.ino
@@ -0,0 +1,29 @@
+void setup() {
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+
+  digitalWrite(10, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, HIGH);
+}
+
+void loop(){
+  // Red light
+  digitalWrite(10, LOW);
+  delay(1000);
+  digitalWrite(10, HIGH);
+  delay(1000);
+
+  // Yellow light
+  digitalWrite(9, LOW);
+  delay(1000);
+  digitalWrite(9, HIGH);
+  delay(1000);
+
+  // Green light
+  digitalWrite(8, LOW);
+  delay(1000);
+  digitalWrite(8, HIGH);
+  delay(1000);
+}
